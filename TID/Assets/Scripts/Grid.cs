0762a8a98862a4ce2db2623ce208f66b1471bc19
Riku Erkkilä <riku.erkkila@gmail.com>
2014-11-28 18:37:11 +0200

    TID prototype added

---
+++ TID/Assets/Scripts/Grid.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void AddBlock( GameObject blockPrefab, int x, int y )
+	{
+		GameObject block = Instantiate (blockPrefab) as GameObject;
+		
+		block.transform.parent = transform;
+		block.transform.localPosition = new Vector3 (x, y, 0);
+		
+		//Block blockScript = block.GetComponent<Block>();
+		//blockScript.xGrid = x;
+		//blockScript.yGrid = y;
+	}
+}
---
+++ TID/Assets/Scripts/WorldCreator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldCreator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		CreateWorld();
+	}
+
+	void CreateWorld()
+	{
+		GameObject worldPrefab = Resources.Load<GameObject>("World");
+		GameObject world = Instantiate (worldPrefab) as GameObject;
+		
+		GameObject dirtPrefab = Resources.Load<GameObject>("Dirt");
+		
+		Grid worldGrid = world.GetComponent<Grid>();
+		
+		worldGrid.AddBlock (dirtPrefab, 0,  2 );
+		worldGrid.AddBlock (dirtPrefab, 0,  1 );
+
+	}
+	
+
+}
